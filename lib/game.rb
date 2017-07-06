90423bce7531b57cec2d27b58830108e385e199f
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-06 20:13:27 +0100

    factor out options list to game class

--- lib/game.rb
+++ lib/game.rb
@@ -10,8 +10,12 @@ class Game
     @options = [correct_definition, incorrect_definition_1, incorrect_definition_2].shuffle
   end
 
+  def list_options
+    "what is the meaning of the word '#{word}'\nis it:\n1. #{options[0]}\n2. #{options[1]}\n3. #{options[2]}"
+  end
+
   def guess(number)
-    options[number-1] == correct_definition ? 'correct!' : "wrong answer. '#{word}' means '#{correct_definition}'"
+    options[(number.to_i)-1] == correct_definition ? 'correct!' : "wrong answer. '#{word}' means '#{correct_definition}'"
   end
 
 end
--- run.rb
+++ run.rb
@@ -3,14 +3,8 @@ require './lib/word.rb'
 
 game = Game.new(Word.new, Word.new, Word.new)
 
-puts "what is the meaning of the word '#{game.word}'"
-
-puts "is it:"
-
-puts "1. #{game.options[0]}"
-puts "2. #{game.options[1]}"
-puts "3. #{game.options[2]}"
+puts game.list_options
 
 selection = gets.chomp
 
-puts game.guess(selection.to_i)
+puts game.guess(selection)
